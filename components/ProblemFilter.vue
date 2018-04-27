1a87d2c10474280937446aeba4a605e359146266
Luma <tomorinao.info@gmail.com>
2018-04-27 23:48:36 +0900

    make problem-filter component

---
+++ components/ProblemFilter.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+v-card
+  v-card-title 検索
+  v-divider
+  v-card-text
+    v-checkbox(
+      v-model.lazy="hide_ac"
+      hide-details
+      label="未ACのみ表示"
+      @change="b => { setHideAC(b) }"
+      )
+    v-checkbox(
+      v-model.lazy="hide_other"
+      hide-details
+      label="自分の問題のみを表示"
+      @change="b => { setHideAC(b) }"
+      )
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState([
+      'hide_ac', 'hide_other'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'setHideAC', 'setHideOther'
+    ])
+  }
+}
+</script>
+
